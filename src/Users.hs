b42acef9741943ae67d65bc217a9bf88721b8646
Mihai Maruseac <mihai@rosedu.org>
2012-07-29 17:41:37 +0300

    Record nicks and nick changes.
    Nicks are recorded when entering, leaving or banning.

--- src/Users.hs
+++ src/Users.hs
@@ -1,21 +1,57 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Users where
 
-import Data.ByteString
 import Data.ByteString.Char8 as BSC
 import Data.Conduit
 import Data.Conduit.List
 import Prelude as P
+import Debug.Trace
 
 import LogReader
+
+data SeenUsers = SU
+  { nicksSeen :: ![String]
+  , nickChanges :: ![(String, String)]
+  } deriving (Show)
+
+noSeenUsers = SU [] []
 
 collectUsers :: FilePath -> IO ()
 collectUsers logDir = do
   logs <- readLogs logDir
-  results <- runResourceT $ logs $$ processLogs
+  results <- runResourceT $ logs $$ fold processLogs noSeenUsers
   print results
 --  P.mapM_ (P.mapM_  (P.putStrLn . ) results
 
-processLogs = awaitForever forEachLine =$ consume
+processLogs :: SeenUsers -> ByteString -> SeenUsers
+processLogs seen line
+  | typ == pack "***" = record content seen
+  | otherwise = seen
+  where
+    (_:typ:content) = BSC.words line
 
-forEachLine l = do
-  yield $ BSC.words l
+record :: [ByteString] -> SeenUsers -> SeenUsers
+record (n:ws) su@SU{..}
+  | pack "has quit" `isPrefixOf` rest = recordOneNick n su
+  | pack "has joined" `isPrefixOf` rest = recordOneNick n su
+  | pack "has left" `isPrefixOf` rest = recordOneNick n su
+  | pack "was kicked by" `isPrefixOf` rest = recordOneNick n su
+  | pack "is now known as" `isPrefixOf` rest = recordChangeNick n (P.last ws) su
+  | pack "sets mode" `isPrefixOf` rest = su
+  | pack "changes topic" `isPrefixOf` rest = su
+  | otherwise = error $ "Unknown action: " ++ show rest
+  where
+    rest = BSC.unwords ws
+
+recordOneNick :: ByteString -> SeenUsers -> SeenUsers
+recordOneNick n su@SU{..} = su { nicksSeen = BSC.unpack n : nicksSeen }
+
+recordChangeNick :: ByteString -> ByteString -> SeenUsers -> SeenUsers
+recordChangeNick nfrom nto su@SU{..} = su
+  { nicksSeen = nick:nick':nicksSeen
+  , nickChanges = (nick, nick') : nickChanges
+  }
+  where
+    nick = BSC.unpack nfrom
+    nick' = BSC.unpack nto
